20f579866d3df5169e17f6bd2329196e314c4c0a
Richin Jain <rijai@microsoft.com>
2016-07-20 17:52:32 -0400

    Adding files for LUIS and Form
    Adding files for LUIS and form.

---
+++ CharlieBot/CharlieBot/MBTARouteInfo.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Bot.Builder.FormFlow;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharlieBot
+{
+
+    [Serializable]
+    public class MBTARouteInfo
+    {
+#pragma warning disable 1998
+        public static IForm<MBTARouteInfo> MakeForm()
+        {
+            FormBuilder<MBTARouteInfo> _Order = new FormBuilder<MBTARouteInfo>();
+            return _Order
+                .Message("Welcome to the Charlie Bot!")
+                .Field(nameof(MBTARouteInfo.StopName))
+                .Field(nameof(MBTARouteInfo.mode))
+                .Field(nameof(MBTARouteInfo.routeName))
+                .AddRemainingFields()
+                .Confirm("You want to know about {mode} from {StopName} for route: {routeName}? ")
+                .OnCompletion(async (session, MBTARouteInfo) =>
+                {
+                    Debug.WriteLine("{0}", MBTARouteInfo);
+                })
+                .Build();
+        }
+
+
+        [Prompt("Which Stock Ticker are you interested in? {||}")]
+        [Describe("Stock Ticker, example: MSFT")]
+        public string StopName;
+
+        [Prompt("Which MBTA service do you want to use?  {||}")]
+        [Describe("MBTA service, example : Bus, train, commuter rail")]
+        public string mode;
+
+        [Prompt("Which route are you interested to? {||}")]
+        [Describe("Route Name, example: 77, Red, Fitchburg")]
+        public int routeName;
+    };
+}
---
+++ CharlieBot/CharlieBot/luis.cs
@@ -0,0 +1,58 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharlieBot
+{
+
+
+    public class CharlieLUIS
+    {
+        public static async Task<CharlieBotLUIS> ParseUserInput(string strInput)
+        {
+            string strRet = string.Empty;
+            string strEscaped = Uri.EscapeDataString(strInput);
+
+            using (var client = new HttpClient())
+            {
+                string uri = "https://api.projectoxford.ai/luis/v1/application?id=5b0818bf-1a55-4b04-b1bd-3e49a277f7e5&subscription-key=2b8c08aa30234d77a0b868503207c24b" + strEscaped;
+                HttpResponseMessage msg = await client.GetAsync(uri);
+
+                if (msg.IsSuccessStatusCode)
+                {
+                    var jsonResponse = await msg.Content.ReadAsStringAsync();
+                    var _Data = JsonConvert.DeserializeObject<CharlieBotLUIS>(jsonResponse);
+                    return _Data;
+                }
+            }
+            return null;
+        }
+    }
+
+    public class CharlieBotLUIS
+    {
+        public string query { get; set; }
+        public lIntent[] intents { get; set; }
+        public lEntity[] entities { get; set; }
+    }
+
+    public class lIntent
+    {
+        public string intent { get; set; }
+        public float score { get; set; }
+    }
+
+    public class lEntity
+    {
+        public string entity { get; set; }
+        public string type { get; set; }
+        public int startIndex { get; set; }
+        public int endIndex { get; set; }
+        public float score { get; set; }
+    }
+
+}
