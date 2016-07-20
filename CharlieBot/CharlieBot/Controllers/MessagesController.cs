1062d6eb7bf496a46dba737dc20d3c205112a425
pauloka <paul.oka@microsoft.com>
2016-07-20 14:41:14 -0400

    Modified Post to call CatchItAPI
    Send the message "stop place-knncl route red" you will get real time
    data back.

--- CharlieBot/CharlieBot/Controllers/MessagesController.cs
+++ CharlieBot/CharlieBot/Controllers/MessagesController.cs
@@ -7,12 +7,15 @@ using System.Web.Http;
 using System.Web.Http.Description;
 using Microsoft.Bot.Connector;
 using Newtonsoft.Json;
+using CatchIt.Data;
+using System.Collections.Generic;
 
 namespace CharlieBot
 {
     [BotAuthentication]
     public class MessagesController : ApiController
     {
+#if false
         /// <summary>
         /// POST: api/Messages
         /// Receive a message from a user and reply to it
@@ -36,6 +39,7 @@ namespace CharlieBot
             var response = Request.CreateResponse(HttpStatusCode.OK);
             return response;
         }
+#endif
 
         private Activity HandleSystemMessage(Activity message)
         {
@@ -65,5 +69,134 @@ namespace CharlieBot
 
             return null;
         }
+
+#if true
+        // ---------------------------------------------- Paul's Code -----------------------------------------
+        // <summary>
+        /// POST: api/Messages
+        /// Receive a message from a user and reply to it
+        /// </summary>
+        public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
+        {
+            if (activity.Type == ActivityTypes.Message)
+            {
+                ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
+                // Get Users specific state data
+                StateClient stateClient = activity.GetStateClient();
+                BotData userData = await stateClient.BotState.GetUserDataAsync(activity.ChannelId, activity.From.Id);
+                string stop = GetId(activity.Text, "stop");
+                string route = GetId(activity.Text, "route");
+
+                if (!string.IsNullOrEmpty(stop))
+                {
+                    // Set StopID State
+                    userData.SetProperty<string>("StopId", stop);
+                    await stateClient.BotState.SetUserDataAsync(activity.ChannelId, activity.From.Id, userData);
+                }
+
+                if (!string.IsNullOrEmpty(route))
+                {
+                    // Set StopID State
+                    userData.SetProperty<string>("RouteId", route);
+                    await stateClient.BotState.SetUserDataAsync(activity.ChannelId, activity.From.Id, userData);
+                }
+
+
+#if false
+                // Set StopID State
+                userData.SetProperty<string>("StopId", "place-knncl");
+                await stateClient.BotState.SetUserDataAsync(activity.ChannelId, activity.From.Id, userData);
+
+                // Set RouteID State
+                userData.SetProperty<string>("RouteId", "64");
+                await stateClient.BotState.SetUserDataAsync(activity.ChannelId, activity.From.Id, userData);
+#endif
+
+                if (userData.GetProperty<string>("StopId") == null || userData.GetProperty<string>("RouteId") == null)
+                {
+                    string errReply = "";
+                    if (userData.GetProperty<string>("StopId") == null)
+                    {
+                        errReply = "What stop do you want to leave from?";
+                    }
+                    else if (userData.GetProperty<string>("RouteId") == null)
+                    {
+                        errReply = "What route do you want to travel?";
+                    }
+                    Activity reply = activity.CreateReply(errReply);
+                    await connector.Conversations.ReplyToActivityAsync(reply);
+                }
+                else
+                {
+                    // Call MBTA API for realtime data
+                    string stopID = userData.GetProperty<string>("StopId");
+                    string stopName = "";
+                    List<string> routeIds = new List<string>();
+                    routeIds.Add(userData.GetProperty<string>("RouteId"));
+                    // Delete save state 
+                    await stateClient.BotState.DeleteStateForUserAsync(activity.ChannelId, activity.From.Id);
+
+
+                    FavoriteDataModel fdm = null;
+                    fdm = await DataManager.Favorites(stopID, stopName, routeIds);
+
+
+                    // Format the replies to users.
+                    if (fdm != null && fdm.Routes != null && fdm.Routes[0].Predictions != null)
+                    {
+                        foreach (RouteDataModel rdm in fdm.Routes)
+                        {
+                            foreach (PredictionDataModel pdm in rdm.Predictions)
+                            {
+                                int cnt = 0;
+                                foreach (Tuple<bool, DateTime> p in pdm.Predictions)
+                                {
+
+                                    TimeSpan diff = p.Item2 - DateTime.Now;
+                                    string mbtaReply = string.Format("The {0} arrives at {1} in {2} mins", pdm.Destination, rdm.StopName, diff.Minutes.ToString()); // "The next train to Braintree arrives in 9 min"
+
+                                    Activity reply = activity.CreateReply(mbtaReply);
+                                    await connector.Conversations.ReplyToActivityAsync(reply);
+
+                                    // Only show 3 predition max.
+                                    if (cnt++ >= 2) break;
+
+                                }
+                            }
+                        }
+                    }
+                    // return our reply to the user
+
+                }
+            }
+            else
+            {
+                HandleSystemMessage(activity);
+            }
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            return response;
+        }
+        private string GetId(string msgText, string searchText)
+        {
+            int i = msgText.IndexOf(searchText);
+
+            if (i < 0) return "";
+            i += searchText.Length + 1;
+
+            string retVal = "";
+            while ((i < msgText.Length) && (msgText[i] != ' '))
+            {
+                retVal += msgText[i];
+                i++;
+            }
+
+            return retVal;
+        }
+
+        private string GetStopId (string stopName)
+        {
+            return "";
+        }
     }
+#endif
 }
